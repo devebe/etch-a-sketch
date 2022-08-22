bc4e826edd029db4380036a5ec4a18a5ed3137cf
devebe <dvbirdja@gmail.com>
2022-08-23 00:14:07 +0200

    Clean up js file

--- JS/etch-a-sketch.js
+++ JS/etch-a-sketch.js
@@ -1,3 +1,30 @@
+let canvasSize = 560;
+
+createHTML('body', 'div', 'container', 1);
+createHTML('div#container', 'div', 'controls', 1);
+createHTML('div#container', 'div', 'canvas', 1);
+createHTML('div#controls', 'button', 'setGrid', 1);
+createHTML('div#controls', 'p', 'title', 1);
+createHTML('div#controls', 'button', 'reset', 1);
+
+
+const setGrid = document.querySelector('button#setGrid');
+setGrid.textContent = 'Set Grid';
+
+const reset = document.querySelector('button#reset');
+reset.textContent = 'Reset All';
+
+const para = document.querySelector('p#title');
+para.textContent = 'Etch-a-sketch'
+
+resetSquares();
+
+setGrid.addEventListener('click', e => {setSquares();});
+
+reset.addEventListener('click', e => {resetSquares();});
+
+
+
 function createGrid (rowAmount, columnAmount, canvasSize) {
     for (let i = 0; i < (parseInt(rowAmount)); i++){
         createHTML('div#canvas', 'div', 'row', 1);
@@ -59,47 +86,19 @@ function randomColor() {
     return color;
 }
 
-function resetSquares () {
+function setSquares() {
+    const canvas = document.querySelector('div#canvas');
+    canvas.remove();
+    createHTML('div#container', 'div', 'canvas', 1);
+    let userInput = getCheckedUserInput();
+    createGrid(userInput, userInput, canvasSize);
+    draw('div#column', '#000000');
+}
+
+function resetSquares() {
     const canvas = document.querySelector('div#canvas');
     canvas.remove();
     createHTML('div#container', 'div', 'canvas', 1);
     createGrid(16, 16, canvasSize);
     draw('div#column', '#000000');
 }
-
-let canvasSize = 560;
-
-createHTML('body', 'div', 'container', 1);
-createHTML('div#container', 'div', 'controls', 1);
-createHTML('div#container', 'div', 'canvas', 1);
-createHTML('div#controls', 'button', 'setGrid', 1);
-createHTML('div#controls', 'p', 'title', 1);
-createHTML('div#controls', 'button', 'reset', 1);
-
-
-const setGrid = document.querySelector('button#setGrid');
-setGrid.textContent = 'Set Grid';
-
-const reset = document.querySelector('button#reset');
-reset.textContent = 'Reset All';
-
-const para = document.querySelector('p#title');
-para.textContent = 'Etch-a-sketch'
-
-
-createGrid(16, 16, canvasSize);
-draw('div#column', '#000000');
-
-setGrid.addEventListener('click', e => {
-    const canvas = document.querySelector('div#canvas');
-    canvas.remove();
-
-    createHTML('div#container', 'div', 'canvas', 1);
-    
-    let userInput = getCheckedUserInput();
-
-    createGrid(userInput, userInput, canvasSize);
-    draw('div#column', '#000000');
-});
-
-reset.addEventListener('click', e => {resetSquares();});
